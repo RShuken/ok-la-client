6f2e7ff0d8fa9d4db41ec2515e0ec427ef41b1c1
rshuken <ryanshuken@gmail.com>
2021-01-26 15:32:42 -0700

    added server endpoints

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://ok-language-learning.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 };
 
 export default config;
-//  ise this one ----  https://ryans-spaced-repetition-server.herokuapp.com/api
+//  ise this one ----  https://ok-language-learning.herokuapp.com/     http://localhost:8000/api
 //'https://cors-anywhere.herokuapp.com/https://ryans-spaced-repetition-server.herokuapp.com/api' ||
