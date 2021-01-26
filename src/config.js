c852e6356b6c2801378a41186539bc751c8219af
rshuken <ryanshuken@gmail.com>
2021-01-26 15:22:38 -0700

    prepare for deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
-export default {
-  API_ENDPOINT:
-    'http://localhost:8000/api',
+const config = {
+  API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 };
+
+export default config;
 //  ise this one ----  https://ryans-spaced-repetition-server.herokuapp.com/api
 //'https://cors-anywhere.herokuapp.com/https://ryans-spaced-repetition-server.herokuapp.com/api' ||
