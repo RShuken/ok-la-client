5a11c1c3af46968f96a0d87b3c4c1510c46aeb53
rshuken <ryanshuken@gmail.com>
2021-01-26 20:49:46 -0700

    finishd style of landingpage

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -15,17 +15,16 @@ class LandingPage extends Component {
           <p>
             Learning a new language can be intimidating, and difficult but it
             doesn't have to be that way. Ok’la is a program which makes
-            remembering new languages easy. 
-            Our flashcard method is more efficient
-            than traditional study methods and can greatly increase the amount
-            you learn. Based on Anki’s spaced repetition flashcards, creating a
-            language deck tracks your progress and show’s you the cards you need
-            to work on the most.
+            remembering new languages easy. Our flashcard method is more
+            efficient than traditional study methods and can greatly increase
+            the amount you learn. Based on Anki’s spaced repetition flashcards,
+            creating a language deck tracks your progress and show’s you the
+            cards you need to work on the most.
           </p>
           <p>
             Create an account and join our language learning community. You can
-            add community decks or create your own decks and
-            share them with everyone.
+            add community decks or create your own decks and share them with
+            everyone.
           </p>
         </div>
       </div>
